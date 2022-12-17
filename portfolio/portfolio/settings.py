e3bcc9931d21b7f15f50ecb9a1dd0e267c9797df
MAHENDAR16 <108650355+MAHENDAR16@users.noreply.github.com>
2022-12-17 11:53:29 +0530

    Add files via upload

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,9 +26,9 @@ STATIC_DIR = os.path.join(BASE_DIR1,'static')
 SECRET_KEY = 'django-insecure-b*c4!tzptw&p4hblit!-&e%g00nizerun2$q*kivgr@e1mbjbw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
